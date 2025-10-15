b1a4bf5ae2afb570eb1f713f409daff92b8a7e82
Joshua Ong <josh.ong2005@gmail.com>
2025-10-15 09:13:15 +0800

    FDGHJKL.

--- assets/forestobjects.tsx
+++ assets/forestobjects.tsx
@@ -86,19 +86,19 @@
  </tile>
  <tile id="18">
   <properties>
-   <property name="type" value="archer"/>
+   <property name="type" value="warrior"/>
   </properties>
   <image source="3 Objects/Grass/1.png" width="17" height="18"/>
  </tile>
  <tile id="19">
   <properties>
-   <property name="type" value="archer"/>
+   <property name="type" value="warrior"/>
   </properties>
   <image source="3 Objects/Grass/2.png" width="17" height="16"/>
  </tile>
  <tile id="20">
   <properties>
-   <property name="type" value="archer"/>
+   <property name="type" value="warrior"/>
   </properties>
   <image source="3 Objects/Grass/3.png" width="14" height="16"/>
  </tile>
@@ -110,37 +110,37 @@
  </tile>
  <tile id="22">
   <properties>
-   <property name="type" value="warrior"/>
+   <property name="type" value="archer"/>
   </properties>
   <image source="3 Objects/Grass/5.png" width="11" height="13"/>
  </tile>
  <tile id="23">
   <properties>
-   <property name="type" value="warrior"/>
+   <property name="type" value="archer"/>
   </properties>
   <image source="3 Objects/Grass/6.png" width="11" height="9"/>
  </tile>
  <tile id="24">
   <properties>
-   <property name="type" value="warrior"/>
+   <property name="type" value="archer"/>
   </properties>
   <image source="3 Objects/Grass/7.png" width="7" height="14"/>
  </tile>
  <tile id="25">
   <properties>
-   <property name="type" value="warrior"/>
+   <property name="type" value="archer"/>
   </properties>
   <image source="3 Objects/Grass/8.png" width="8" height="14"/>
  </tile>
  <tile id="26">
   <properties>
-   <property name="type" value="warrior"/>
+   <property name="type" value="archer"/>
   </properties>
   <image source="3 Objects/Grass/9.png" width="6" height="8"/>
  </tile>
  <tile id="27">
   <properties>
-   <property name="type" value="warrior"/>
+   <property name="type" value="archer"/>
   </properties>
   <image source="3 Objects/Grass/10.png" width="9" height="11"/>
  </tile>
@@ -155,7 +155,7 @@
  </tile>
  <tile id="30">
   <properties>
-   <property name="type" value="end"/>
+   <property name="type" value=""/>
   </properties>
   <image source="3 Objects/Pointers/3.png" width="13" height="21"/>
  </tile>
